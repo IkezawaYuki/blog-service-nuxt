c2a7e0d267e7bd1fe5423e55a0ab9a2b9a33258c
yikezawa <y.ikezawa93@gmail.com>
2019-09-28 17:02:06 +0900

    add post function

--- app/store/posts.js
+++ app/store/posts.js
@@ -21,9 +21,31 @@ export const mutations = {
 }
 
 export const actions = {
+  async fetchPost({commit}){
+    const posts = await this.$axios.$get(`/posts.json`)
+    commit('clearPosts')
+    Object.entries(posts)
+      .reverse()
+      .forEach(([id, content]) =>
+        commit('addPost', {
+          post:{
+            id,
+            ...content
+          }
+        })
+      )
+  },
   async publishPost({commit}, {payload}){
     const user = await this.$axios.$get(`/users/${payload.user.id}.json`)
     const post_id = (await this.$axios.$post('/posts.json', payload)).name
-    
+    const created_at = moment().format()
+    const post = {id: post_id, ...payload, created_at}
+    const putData = {id: post_id, ...payload, created_at}
+    delete putData.user
+    await this.$axios.$put(`/users/${user.id}/posts.json`,[
+      ...(user.posts || []),
+      putData
+    ])
+    commit('addPost', {post})
   }
 }
